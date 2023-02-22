8a9215ba2f774ef2d9107bf8534586bfa189d45d
DaySlacker <111376118+AshlynBarrera@users.noreply.github.com>
2023-02-22 08:42:24 -0600

    Initial Commit

---
+++ RockPaperScissorsGame/RockPaperScissorsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsGameApp.swift
+//  RockPaperScissorsGame
+//
+//  Created by Ashlyn Barrera on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
