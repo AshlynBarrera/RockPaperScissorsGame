cceb74ba86efcf32d1b4de28139b2551d07d58a0
DaySlacker <111376118+AshlynBarrera@users.noreply.github.com>
2023-02-22 09:15:42 -0600

    Created all content; added background color as well as changes color on button to white.

--- RockPaperScissorsGame/ContentView.swift
+++ RockPaperScissorsGame/ContentView.swift
@@ -9,15 +9,67 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            // ignoresSafeArea() allows the color to fill the screen of VP
+            Color.blue.ignoresSafeArea()
+            
+            VStack{
+                Text("Rock Paper Sciccors Game")
+                
+                Text("Select Rock,Paper, or Scissor to start the game")
+                
+                HStack{
+                    Button {
+                        print("Button Clicked")
+                    } label: {
+                        Text("🗿")
+                    }
+                    
+                    Button {
+                        print("Button Clicked")
+                    } label: {
+                        Text("📜")
+                    }
+                    
+                    Button {
+                        print("Button Clicked")
+                    } label: {
+                        Text("✂️")
+                    }
+                    
+                }
+                ////////////////////////
+                HStack{
+                    VStack{
+                        Text("🗿")
+                        Text("Player")
+                        Text("0")
+                    }
+                    //
+                    //
+                    VStack{
+                        Text("🗿")
+                            
+                        Text("Player")
+                        Text("0")
+                        
+                    }
+                    //
+                    
+                    }
+                Button {
+                    print("Button Clicked")
+                } label: {
+                    Text("Restart Game")
+                        .font(.largeTitle)
+                        .foregroundColor(Color.white)
+                    
+                   }
+                }
+            }
         }
-        .padding()
+
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
