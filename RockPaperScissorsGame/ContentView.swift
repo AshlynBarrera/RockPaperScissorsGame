3fd33acc44ea47cc8fa85ba2ecb326dee09110e0
DaySlacker <111376118+AshlynBarrera@users.noreply.github.com>
2023-02-23 08:22:35 -0600

    Finished initial styling for all elements,

--- RockPaperScissorsGame/ContentView.swift
+++ RockPaperScissorsGame/ContentView.swift
@@ -15,26 +15,44 @@ struct ContentView: View {
             
             VStack{
                 Text("Rock Paper Sciccors Game")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
                 
                 Text("Select Rock,Paper, or Scissor to start the game")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
                 
                 HStack{
                     Button {
                         print("Button Clicked")
                     } label: {
                         Text("🗿")
+                            .font(.system(size: 75))
+                            .padding(.bottom, 10.0)
+                            // using .system(size: int)
+                        //allows foe custom size.
+                            
+                            
                     }
                     
                     Button {
                         print("Button Clicked")
                     } label: {
                         Text("📜")
+                            .font(.system(size: 75))
+                            .padding(.bottom, 10.0)
                     }
                     
                     Button {
                         print("Button Clicked")
                     } label: {
                         Text("✂️")
+                            .font(.system(size: 75))
+                            .padding(.bottom, 10.0)
                     }
                     
                 }
@@ -42,16 +60,28 @@ struct ContentView: View {
                 HStack{
                     VStack{
                         Text("🗿")
+                            .font(.system(size: 55))
+                            .padding(.bottom, 10.0)
                         Text("Player")
+                            .font(.title)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        
                         Text("0")
+                            .foregroundColor(Color.white)
                     }
                     //
                     //
                     VStack{
                         Text("🗿")
-                            
+                            .font(.system(size: 55))
+                            .padding(.bottom, 10.0)
                         Text("Player")
+                            .font(.title)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
                         Text("0")
+                            .foregroundColor(Color.white)
                         
                     }
                     //
@@ -63,6 +93,9 @@ struct ContentView: View {
                     Text("Restart Game")
                         .font(.largeTitle)
                         .foregroundColor(Color.white)
+                        .padding(.all, 10.0)
+                        .border(/*@START_MENU_TOKEN@*/Color.white/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/5/*@END_MENU_TOKEN@*/)
+                        .cornerRadius(/*@START_MENU_TOKEN@*/8.0/*@END_MENU_TOKEN@*/)
                     
                    }
                 }
